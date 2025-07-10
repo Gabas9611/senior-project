a61e57a64082728959aef40f7c27357a98c30b72
Gabas9611 <gabas9611@gmail.com>
2025-07-10 22:38:44 +0800

    Update taiwan-history.js

--- js/taiwan-history.js
+++ js/taiwan-history.js
@@ -12,11 +12,11 @@ createApp({
             console.log('Button clicked with action:', action);
             this.selectedAction = action;
             if (action === 'home') {
-                console.log('Redirecting to loading畫面.html with target index.html');
+                console.log('Redirecting to loading.html with target index.html');
                 window.location.href = 'loading.html?target=index.html';
                 this.actionMessage = '返回首頁已點擊';
             } else if (action === 'previous') {
-                console.log('Redirecting to loading畫面.html with target 主題頁面.html');
+                console.log('Redirecting to loading.html with target topic.html');
                 window.location.href = 'loading.html?target=topic.html';
                 this.actionMessage = '返回前頁已點擊';
             }
