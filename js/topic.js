cfe993357c78d308052678cf8b53123e77dd60ee
Gabas9611 <gabas9611@gmail.com>
2025-07-13 23:09:57 +0800

    Update topic.js

--- js/topic.js
+++ js/topic.js
@@ -629,27 +629,27 @@ createApp({
         // 導覽攝影機的設定 (保持不變)
         cameraNav1 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         cameraNav1.name = "NavCamera1";
-        cameraNav1.position.set(-6.55, 1.25, 1.06);
+        cameraNav1.position.set(-6.55, 0.65, -6.91);
 
         cameraNav2 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         cameraNav2.name = "NavCamera2";
-        cameraNav2.position.set(-4.30,  1.25, 1.06);
+        cameraNav2.position.set(-4.30,  0.65, -6.91);
 
         cameraNav3 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         cameraNav3.name = "NavCamera3";
-        cameraNav3.position.set(-2.03,  1.25, 1.06);
+        cameraNav3.position.set(-2.03,  0.65, -6.91);
 
         cameraNav4 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         cameraNav4.name = "NavCamera4";
-        cameraNav4.position.set(-0.53,  1.25, 1.95);
+        cameraNav4.position.set(-0.53,  0.65, -6.02);
 
         cameraNav5 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         cameraNav5.name = "NavCamera5";
-        cameraNav5.position.set(1.67,  1.25, 1.06);
+        cameraNav5.position.set(1.67,  0.65, -6.91);
 
         cameraNav6 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         cameraNav6.name = "NavCamera6";
-        cameraNav6.position.set(3.93,  1.25, 1.92);
+        cameraNav6.position.set(3.93,  0.65, -6.02);
 
 
 
