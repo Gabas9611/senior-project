1b4fcaa661c706869b978412d2f1c002b7c4c229
dennyxian <zzx08516@yahoo.com>
2025-07-14 11:00:48 +0800

    調整

--- js/traffic-information.js
+++ js/traffic-information.js
@@ -1,159 +1,21 @@
-const { createApp, ref, onMounted, onUnmounted } = Vue;
+const { createApp, onMounted, nextTick } = Vue;
 
-const app = createApp({
-    setup() {
-        const selectedAction = ref('');
-        const actionMessage = ref('');
-        const centerIndex = ref(2);
-        const isMapVisible = ref(false);
-        const screenWidth = ref(window.innerWidth);
-        const isInteractionDisabled = ref(false); // New: Flag to disable interaction
+        createApp({
+            setup() {
+                // 處理導航點擊事件
+                const handleNavClick = (action) => {
+                    switch (action) {
+                        case 'previous':
+                            // 方法1: 導航到首頁
+                            window.location.href = 'topic.html';
+                            break;
+                        default:
+                            console.log('未知的導航動作:', action);
+                    }
+                };
 
-        const boxes = ref([
-            {
-                title: '單車',
-                title_name: '單車(29min)',
-                text:
-                    '從大智北一街左轉至大智北路，右轉進新民街，右轉進入復興路四段，左轉進入復興東路，右轉進入樂業路，左轉進入祥順路一段，向右轉進入新平路一段，向左轉進入環太東路，向左轉進入大源路，向右轉進入中山路二段477巷186弄，向右轉進入明秀西路，向左轉進入明秀南路，右轉即可到達目的地。',
-                img: './img/traffic/bike.jpg',
-            },
-            {
-                title: '241公車',
-                title_name: '241公車(39min)',
-                text: '從台中火車站步行4min至台中車站C月台轉乘，經過31min至三中心站下車，再步行4min到達目的地。',
-                img: './img/traffic/241bus.jpg',
-            },
-            {
-                title: '汽車',
-                title_name: '汽車(22min)',
-                text: '從復興路四段到復興東路，向右轉進樂業路，左轉進入祥順路一段，右轉進入中山路二段，之後右轉進入明秀北路，左轉進入明秀東路，再左轉即可到達目的地。',
-                img: './img/traffic/car.jpg'
-            },
-            {
-                title: '51公車',
-                title_name: '51公車(32min)',
-                text: '從台中火車站步行3min到新時代購物中心站搭公車經過24min到三中心站下車，再步行5min到達目的地。',
-                img: './img/traffic/51bus.jpg',
-            },
-            {
-                title: '41公車',
-                title_name: '41公車(40min)',
-                text: '從台中火車站步行6min到第一廣場轉乘，經過29min後在三中心站下車，再步行5min後抵達。',
-                img: './img/traffic/41bus.jpg',
+                return {
+                    handleNavClick,
+                };
             }
-        ]);
-
-        const getMaxAllowedOffset = () => {
-            if (screenWidth.value <= 425) {
-                return 90;
-            }
-            else if (screenWidth.value <= 480) {
-                return 180;
-            }
-            else if (screenWidth.value <= 768) {
-                return 250;
-            } else {
-                return 450;
-            }
-        };
-
-        const getCardSpacing = () => {
-            if (screenWidth.value <= 480) {
-                return 120;
-            } else if (screenWidth.value <= 768) {
-                return 160;
-            } else {
-                return 200;
-            }
-        };
-
-        const handleResize = () => {
-            screenWidth.value = window.innerWidth;
-        };
-        const handleNavClick = (action) => {
-            console.log('Button clicked with action:', action);
-            selectedAction.value = action;
-            if (action === 'home') {
-                window.location.href = 'index.html';
-                actionMessage.value = '返回首頁已點擊';
-            } else if (action === 'previous') {
-                window.location.href = 'topic.html';
-                actionMessage.value = '返回前頁已點擊';
-            }
-        };
-
-        const toggleMap = () => {
-            isMapVisible.value = !isMapVisible.value;
-        };
-
-        const updateLayout = (index) => {
-            if (isInteractionDisabled.value) {
-                return; // If interaction is disabled, do nothing
-            }
-
-            centerIndex.value = index;
-            isInteractionDisabled.value = true; // Disable interaction
-            setTimeout(() => {
-                isInteractionDisabled.value = false; // Re-enable interaction after 1 second
-            }, 1000); // 1000 milliseconds = 1 second
-        };
-
-        const getSizeClass = (i) => {
-            const diff = Math.abs(i - centerIndex.value);
-            if (diff === 0) return 'large';
-            else if (diff === 1) return 'medium';
-            return 'small';
-        };
-
-        const getStyle = (i) => {
-            const totalBoxes = boxes.value.length;
-            const cardSpacing = getCardSpacing();
-            const maxAllowedOffset = getMaxAllowedOffset();
-
-            let offset = (i - centerIndex.value) * cardSpacing;
-
-            if (Math.abs(offset) > maxAllowedOffset) {
-                offset = offset > 0 ? maxAllowedOffset : -maxAllowedOffset;
-            }
-
-            if (centerIndex.value === 0) {
-                if (i > centerIndex.value) {
-                    offset = Math.min(offset, maxAllowedOffset);
-                }
-            } else if (centerIndex.value === totalBoxes - 1) {
-                if (i < centerIndex.value) {
-                    offset = Math.max(offset, -maxAllowedOffset);
-                }
-            }
-
-            return {
-                transform: `translate(-50%, -50%) translateX(${offset}px)`
-            };
-        };
-
-        onMounted(() => {
-            window.addEventListener('resize', handleResize);
-        });
-
-        onUnmounted(() => {
-            window.removeEventListener('resize', handleResize);
-        });
-
-        return {
-            selectedAction,
-            actionMessage,
-            centerIndex,
-            boxes,
-            isMapVisible,
-            screenWidth,
-            handleNavClick,
-            toggleMap,
-            updateLayout,
-            getSizeClass,
-            getStyle
-        };
-    }
-});
-
-// 掛載 Vue 應用
-app.mount('#app');
+        }).mount("#app");
