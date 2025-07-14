ead21d9330ff1d22bb70c51a6872d09c2f52ca30
Gabas9611 <gabas9611@gmail.com>
2025-07-14 20:36:55 +0800

    Update traffic-information.js

--- js/traffic-information.js
+++ js/traffic-information.js
@@ -1,33 +1,44 @@
- const { createApp, onMounted, nextTick } = Vue;
-        createApp({
-            setup() {
-                // 處理導航點擊事件
-                const handleNavClick = (action) => {
-                    switch (action) {
-                        case 'previous':
-                            // 方法1: 導航到首頁
-                            window.location.href = 'topic.html';
-                            break;
-                        default:
-                            console.log('未知的導航動作:', action);
-                    }
-                };
+const { createApp, onMounted, nextTick } = Vue;
+createApp({
+    setup() {
+        const handleNavClick = (action) => {
+            switch (action) {
+                case 'previous':
+                    window.location.href = 'topic.html';
+                    break;
+                default:
+                    console.log('未知的導航動作:', action);
+            }
+        };
 
-
-                onMounted(async () => {
-                    await nextTick();
-                    new Swiper(".mySwiper", {
+        onMounted(async () => {
+            await nextTick();
+            new Swiper(".mySwiper", {
+                slidesPerView: 3,
+                spaceBetween: 30,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+                breakpoints: {
+                    0: {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+                    480: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    768: {
                         slidesPerView: 3,
                         spaceBetween: 30,
-                        pagination: {
-                            el: ".swiper-pagination",
-                            clickable: true,
-                        },
-                    });
-                });
-                return {
-                    handleNavClick,
-                };
+                    },
+                },
+            });
+        });
 
-            }
-        }).mount("#app");
+        return {
+            handleNavClick,
+        };
+    }
+}).mount("#app");
